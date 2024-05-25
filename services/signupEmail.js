3c4aa1c9da744eade4852e750f1be8dbb6645ab6
Andana Satrio Herdiansah <113823539+AndanaSat@users.noreply.github.com>
2024-05-26 00:25:31 +0700

    Update signupEmail.js

--- services/signupEmail.js
+++ services/signupEmail.js
@@ -23,8 +23,4 @@ async function signupEmail(email, password, callback) {
         });
     }
 }
-<<<<<<< HEAD
 module.exports = signupEmail;
-=======
-module.exports = signupEmail;
->>>>>>> 54c0bf48f87aaf9ca02456d77325af87e7770833

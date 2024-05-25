54c0bf48f87aaf9ca02456d77325af87e7770833
Andana Satrio Herdiansah <113823539+AndanaSat@users.noreply.github.com>
2024-05-26 00:17:53 +0700

    Update signupEmail.js

--- services/signupEmail.js
+++ services/signupEmail.js
@@ -1,13 +1,26 @@
-const { auth } = require('../config/firebase');
+const auth = require('../config/firebase');
 const { createUserWithEmailAndPassword } = require('firebase/auth');
 
-async function signupEmail(email, password) {
+async function signupEmail(email, password, callback) {
     try {
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        return userCredential;
+        await createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            callback({
+                'uid': userCredential.user.uid,
+                'email': userCredential.user.email
+            })
+        })
+        .catch((error) => {
+            callback ({
+                'status': 'fail',
+                'message': error.message || 'Signup failed'
+            });
+        })
     } catch (error) {
-        return error;
+        callback ({
+            'status': 'fail',
+            'message': error.message || 'Signup failed'
+        });
     }
 }
-
 module.exports = signupEmail;

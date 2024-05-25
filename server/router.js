ece67c001ae76f8b070556b27ef4cd8247607037
Andana Satrio Herdiansah <113823539+AndanaSat@users.noreply.github.com>
2024-05-26 00:16:50 +0700

    Update router.js

--- server/router.js
+++ server/router.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const router = express.Router()
-const getWord = require('./handler')
-const bodyParser = require('body-parser')
-const {loginEmail} = require('../services/loginEmail') // Login Function
-const {signupEmail} = require('../services/signupEmail') // Signup Function
+const express = require('express');
+const router = express.Router();
+const getWord = require('./handler');
+const signupEmail = require('../services/signupEmail');
+const loginEmail = require('../services/loginEmail');
 
 router.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-router.get('/word/:word', (req, res) => {
+router.get('/word/:word', async (req, res) => {
     let { word } = req.params;
     try{
-        getWord(word, (data) => {
+        await getWord(word, (data) => {
             res.status(200);
 
             if(data.status === 'fail'){
@@ -25,17 +24,15 @@ router.get('/word/:word', (req, res) => {
     catch(error){
         res.send({
             'status': 'fail',
-            'messagge': 'gagal get word'
+            'message': 'gagal get word'
         }).status(404);
     }
 })
 
-router.post('/email/login', (req, res)=>{
+router.post('/email/login', async (req, res)=>{
     let { email, password } = req.body;
     try{
-        loginEmail(email, password)
-        .then((userCredential) => {
-            // res.send(userCredential).status(200);
+        await loginEmail(email, password, (userCredential) => {
             res.send({
                 status: 'success',
                 message: 'Login berhasil',
@@ -47,31 +44,28 @@ router.post('/email/login', (req, res)=>{
             'status': 'fail',
             'message': 'Terjadi kesalahan silahkan cek kembali email dan password anda'
         }).status(404);
+        console.log(error)
     }
 })
 
-router.post('/email/signup', (req, res)=>{
+router.post('/email/signup', async (req, res)=>{
     let email = req.body.email;
     let password = req.body.password;
-    console.log(email, password);
-    try{
-        signupEmail(email, password)
-        .then((userCredential) => {
-            // res.send(userCredential).status(200);
+
+    try {
+        await signupEmail(email, password, (userCredential) => {
             res.send({
                 status: 'success',
                 message: 'Signup berhasil',
                 data: userCredential
             }).status(200);
         })
-
     } catch (error) {
-        res.send({
-            'status': 'fail',
-            'message': 'Terjadi kesalahan silahkan cek kembali email dan password anda'
-        }).status(404);
+        res.status(400).send({ // Use 400 for bad request (e.g., invalid email)
+            status: 'fail',
+            message: error.message || 'Signup failed' // Provide informative message
+        });
     }
 })
-
 
 module.exports = router;

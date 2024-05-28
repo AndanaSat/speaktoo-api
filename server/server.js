c9f8b2d9ae22c19b56f83641ec33dc3e36f5fd74
aud1tya4dnan <auditya.adnan2k3@gmail.com>
2024-05-28 15:14:44 +0700

    using .env

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const router = require('./router')
 const bodyParser = require('body-parser')
+require('dotenv').config()
 
 app.use(bodyParser.json())
 app.use(router)

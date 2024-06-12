b98615942cede9e659144475cf5bb9408fea866b
Andana Satrio Herdiansah <113823539+AndanaSat@users.noreply.github.com>
2024-06-12 20:01:55 +0700

    Update sqlServices.js

--- services/sqlServices.js
+++ services/sqlServices.js
@@ -1,28 +1,5 @@
 const db = require('../config/sqlConfig');
 
-async function postUserProgress(user_id, username, callback){
-    try {
-        const sql = 'INSERT INTO user_progress (user_id, username) VALUES (?, ?)';
-        await db.query(sql, [user_id, username]);
-        return username;
-    } catch (error) {
-        console.log(error);
-        return 'fail';
-    }
-}
-
-async function getUserProgress(user_id){
-    try{
-        const sql = 'SELECT * FROM user_progress WHERE user_id = ?';
-        const [result] = await db.query(sql, [user_id]);
-        return result[0];
-    } catch (error) {
-        console.log(error);
-        return 'fail';
-    }
-    
-}
-
 async function updateUserProgress(user_id, progress){
     try {
         const sql = 'UPDATE user_progress SET progress = ? WHERE user_id = ?';
@@ -71,35 +48,9 @@ async function postUserLogs(user_id, word_id){
     }
 }
 
-async function editUserUsername (user_id, username){
-    try {
-        const sql = 'UPDATE user_progress SET username = ? WHERE user_id = ?';
-        await db.query(sql, [username, user_id]);
-        return username;
-    } catch (error) {
-        console.log(error);
-        return 'fail';
-    }
-}
-
-async function addUserProfilePic(user_id, url){
-    try {
-        const sql = 'UPDATE user_progress SET profile_pic = ? WHERE user_id = ?';
-        await db.query(sql, [url, user_id]);
-        return url;
-    } catch (error) {
-        console.log(error)
-        return 'fail';
-    }
-}
-
 module.exports = { 
-    postUserProgress, 
-    getUserProgress, 
     updateUserProgress, 
     getWords, 
     getCompletedWords, 
-    postUserLogs,
-    editUserUsername,
-    addUserProfilePic
+    postUserLogs
 };

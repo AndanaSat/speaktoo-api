3b108fbd4953a8368481a7ae57a20d8d30ca0835
Andana Satrio Herdiansah <113823539+AndanaSat@users.noreply.github.com>
2024-06-12 20:00:43 +0700

    Update router.js

--- server/router.js
+++ server/router.js
@@ -4,15 +4,9 @@ const multer = require('multer');
 
 const { 
     getWord, 
-    loginUser, 
-    signupUser, 
     updateProgress,  
     getWordsByDifficulty, 
-    postLogs, 
-    userForgetPassword,
-    editUsername,
-    uploadProfilePic,
-    ubahPassword
+    postLogs
 } = require('./handler');
 
 const upload = multer({ 
@@ -21,46 +15,6 @@ const upload = multer({
 
 router.get('/', (req, res) => {
     res.send('Hello World!');
-})
-
-router.post('/email/login', async (req, res)=>{
-    let { email, password } = req.body;
-    try{
-        const data = await loginUser(email, password);
-        res.status(201);
-
-        if(data.status === 'fail'){
-            res.status(404);
-        }
-
-        res.send(data);
-    } catch (error){
-        console.error(error);
-        res.send({
-          'status': 'fail',
-          'message': 'Harap maklum'
-        }).status(500);
-    }
-})
-
-router.post('/email/signup', async (req, res)=>{
-    let { email, password, username } = req.body;
-    try{
-        const data = await signupUser(email, password, username);
-        res.status(201);
-
-        if(data.status === 'fail'){
-            res.status(400);
-        }
-
-        res.send(data);
-    } catch (error) {
-        console.log(error);
-        res.send({
-            'status': 'fail',
-            'message': 'Harap maklum'
-        }).status(500);
-    }
 })
 
 router.put('/user/progress', async (req, res) => {
@@ -134,91 +88,6 @@ router.post('/user/logs', async(req, res) => {
 
         if(data.status === 'fail'){
             res.status(400);
-        }
-
-        res.send(data);
-    } catch (error) {
-        console.log(error);
-        res.send({
-            'status': 'fail',
-            'message': 'harap maklum'
-        }).status(500);
-    }
-})
-
-router.post('/email/forpas', async (req, res) => {
-    let { email } = req.body;
-    try {
-        const data = await userForgetPassword(email);
-        res.send(200);
-
-        if(data.status === 'fail'){
-            res.status(404);
-        }
-
-        res.send(data);
-    } catch (error) {
-        console.log(error);
-        res.send({
-            'status': 'fail',
-            'message': 'harap maklum'
-        }).status(500);
-    }
-})
-
-router.put('/user/username', async (req, res) => {
-    let user_id = req.body.uid;
-    let username = req.body.username;
-    try {
-        const data = await editUsername(user_id, username);
-        res.status(200);
-
-        if(data.status === 'fail'){
-            res.status(404);
-        }
-
-        res.send(data);
-    } catch (error) {
-        console.log(error);
-        res.send({
-            'status': 'fail',
-            'message': 'harap maklum'
-        }).status(500);
-    }
-})
-
-router.post('/user/profile', upload.single('image'), async (req, res) => {
-    let user_id = req.body.uid;
-    let file = req.file;
-    let filename = req.file.originalname;
-    try {
-        const data = await uploadProfilePic(user_id, file, filename);
-        res.status(201);
-
-        if(data.status === 'fail'){
-            res.status(400);
-        }
-        
-        res.send(data);
-    } catch (error) {
-        console.log(error);
-        res.send({
-            'status': 'fail',
-            'message': 'Terjadi Kesalahan Pada Server'
-        }).status(500);
-    }
-})
-
-router.put('/user/pass', async (req, res) => {
-    let email = req.body.email;
-    let oldPassword = req.body.oldPassword;
-    let newPassword = req.body.newPassword;
-    try {
-        const data = await ubahPassword(email, oldPassword, newPassword);
-        res.status(200);
-
-        if(data.status === 'fail'){
-            res.status(404);
         }
 
         res.send(data);

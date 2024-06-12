1cd0921ab121388010b7423d678700fe154b38ce
Andana Satrio Herdiansah <113823539+AndanaSat@users.noreply.github.com>
2024-06-12 19:59:30 +0700

    Update handler.js

--- server/handler.js
+++ server/handler.js
@@ -1,17 +1,9 @@
-const getWordService = require('../services/getWordService')
-const signupEmail = require('../services/signupEmail');
-const {loginEmail, editPassword} = require('../services/loginEmail');
-const forgetPassword = require('../services/forgetPassword');
-const uploadUserProfilePic = require('../services/profileUser');
+const getWordService = require('../services/getWordService');
 const { 
-    postUserProgress, 
-    getUserProgress, 
     updateUserProgress, 
     getWords, 
     getCompletedWords, 
-    postUserLogs,
-    editUserUsername,
-    addUserProfilePic
+    postUserLogs
 } = require('../services/sqlServices');
 
 async function getWord(word) {
@@ -40,68 +32,6 @@ async function getWord(word) {
         return {
             'status': 'fail',
             'message': 'gagal get word'
-        };
-    }
-}
-
-async function loginUser(email, password){
-    try{
-        const userCredential = await loginEmail(email, password);
-        const userCredentialSQL = await getUserProgress(userCredential.uid);
-
-        if(userCredential === 'fail' || userCredentialSQL === 'fail' ){
-            return {
-                'status': 'fail',
-                'message': 'Login failed, silahkan cek kembali email dan password anda'
-            };
-        }
-
-        return {
-            'status': 'success',
-            'message': 'Login berhasil',
-            'data': {
-                'uid': userCredential.uid,
-                'email': userCredential.email,
-                'username': userCredentialSQL.username,
-                'progress': userCredentialSQL.progress,
-                'profile_pic': userCredentialSQL.profile_pic
-            }
-        };
-    }catch(error){
-        console.error(error);
-        return {
-            status: 'fail',
-            message: 'Terjadi kesalahan silahkan cek kembali email dan password anda'
-        };
-    }
-}
-
-async function signupUser(email, password, username){
-    try {
-        const userCredential = await signupEmail(email, password);
-        const userCredentialSQL = await postUserProgress(userCredential.uid, username);
-
-        if(userCredential === 'fail' || userCredentialSQL === 'fail' ){
-            return {
-                'status': 'fail',
-                'message': 'Signup failed, silahkan cek kembali email dan password anda'
-            };
-        }
-
-        return {
-            "status": "success",
-            "message": "Signup berhasil",
-            "data": {
-                'uid': userCredential.uid,
-                'email': userCredential.email,
-                'username': userCredentialSQL
-            }
-        };
-    } catch (error) {
-        console.log(error);
-        return {
-            'status': 'fail',
-            'message': 'Signup failed, silahkan cek kembali email dan password anda'
         };
     }
 }
@@ -195,117 +125,9 @@ async function postLogs(user_id, word_id){
     }
 }
 
-async function userForgetPassword(email) {
-    try {
-        const result = await forgetPassword(email);
-
-        if(result === 'fail'){
-            return {
-                'status': 'fail',
-                'message': 'mohon cek kembali format email anda'
-            };
-        }
-
-        return {
-            'status': 'success',
-	        'message': result
-        };
-    } catch (error) {
-        console.log(error);
-        return {
-            'status': 'fail',
-            'message': 'mohon cek kembali format email anda'
-        };
-    }
-}
-
-async function editUsername(user_id, username) {
-    try {
-        const result = await editUserUsername(user_id, username);
-
-        if(result === 'fail'){
-            return {
-                'status': 'fail',
-                'message': 'mohon cek kembail akun anda'
-            }
-        }
-
-        return {
-            'status': 'success',
-            'message': 'berhasil mengganti username',
-            'data': result
-        };
-    } catch (error) {
-        console.log(error);
-        return {
-            'status': 'fail',
-            'message': 'mohon cek kembail akun anda'
-        };
-    }
-}
-
-async function uploadProfilePic(user_id, file, filename){
-    try {
-        const file_extension = filename.slice(
-            ((filename.lastIndexOf('.') - 1) >>> 0) + 2
-        );
-        
-        const result = await uploadUserProfilePic(user_id, file, file_extension);
-        const resultSQL = await addUserProfilePic(user_id, result);
-
-        if(result === 'fail' || resultSQL === 'fail'){
-            return {
-                'status': 'fail',
-                'message': 'mohon cek kembail file anda'
-            };
-        }
-
-        return {
-            'status': 'success',
-            'message': 'berhasil upload profile picture'
-        };
-    } catch (error) {
-        console.log(error);
-        return {
-            'status': 'fail',
-            'message': 'terjadi kesalahan pada server'
-        };
-    }
-}
-
-async function ubahPassword(email, oldPassword, newPassword){
-    try {
-        const result = await editPassword(email, oldPassword, newPassword);
-
-        if(result === 'fail'){
-            return {
-                'status': 'fail',
-                'message': 'gagal mengganti password'
-            };
-        }
-
-        return {
-            'status': 'success',
-            'message': 'berhasil mengganti password'
-        };
-    } catch (error) {
-        console.log(error);
-        return {
-            'status': 'fail',
-            'message': 'gagal mengganti password'
-        };
-    }
-}
-
 module.exports = { 
     getWord, 
-    loginUser, 
-    signupUser, 
     updateProgress, 
     getWordsByDifficulty, 
-    postLogs, 
-    userForgetPassword,
-    editUsername,
-    uploadProfilePic,
-    ubahPassword
+    postLogs
 };

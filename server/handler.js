5fbd2c22dc7460fe816ce459897c91391e9d4263
Andana Satrio Herdiansah <113823539+AndanaSat@users.noreply.github.com>
2024-06-14 22:00:14 +0700

    Update handler.js

--- server/handler.js
+++ server/handler.js
@@ -1,13 +1,49 @@
+const buffer = require('buffer').Buffer;
+const getWordService = require('../services/getWordService')
 const signupEmail = require('../services/signupEmail');
 const {loginEmail, editPassword} = require('../services/loginEmail');
 const forgetPassword = require('../services/forgetPassword');
 const uploadUserProfilePic = require('../services/profileUser');
 const { 
-    postUserProgress,
+    postUserProgress, 
     getUserProgress, 
+    updateUserProgress, 
+    getWords, 
+    getCompletedWords, 
+    postUserLogs,
     editUserUsername,
     addUserProfilePic
 } = require('../services/sqlServices');
+
+async function getWord(word) {
+    try{
+        const result = await getWordService(word);
+
+        if(result === 'fail'){
+            return {
+                'status': 'fail',
+                'message': 'gagal get word'
+            };
+        }
+
+        return {
+            'status': 'success',
+            'message': 'berhasil get',
+            'data': {
+                'word': word,
+                'audio': encodeURI('https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en&q=' + word),
+                result
+            }
+        };
+    }
+    catch(error) {
+        console.log(error);
+        return {
+            'status': 'fail',
+            'message': 'gagal get word'
+        };
+    }
+}
 
 async function loginUser(email, password){
     try{
@@ -71,6 +107,95 @@ async function signupUser(email, password, username){
     }
 }
 
+async function updateProgress(user_id, progress){
+    try {
+        const result = await updateUserProgress(user_id, progress);
+
+        if(result === 'fail'){
+            return {
+                'status': 'fail',
+                'message': 'gagal update progress'
+            };
+        }
+
+        return {
+            'status' : 'success',
+	        'message' : 'berhasil update progress',
+	        'data' : {
+	            'uid' : result.user_id,
+	            'progress' : result.progress
+            }
+        };
+
+    } catch (error) {
+        console.log(error);
+        return {
+            'status': 'fail',
+            'message': 'gagal update progress'
+        }        
+    }
+}
+
+async function getWordsByDifficulty(user_id, difficulty) {
+    try {
+        const words = await getWords(difficulty);
+        const completedWords = await getCompletedWords(user_id, difficulty);
+
+        if(words === 'fail' || completedWords === 'fail') {
+            return {
+                'status': 'fail',
+                'message': 'gagal get words by difficulty'
+            };
+        }
+
+        const result = [];
+        words.forEach(item => {
+            const found = completedWords.find(element => element.word === item.word);
+            const completed = found ? found.completed : 0;
+            result.push({ ...item, completed });
+        });
+
+        console.log(result);
+
+        return {
+            'status' : 'success',
+	        'message' : 'berhasil get words by difficulty',
+	        'data' : result
+        }
+    } catch (error) {
+        console.log(error);
+        return {
+            'status': 'fail',
+            'message': 'gagal get words by difficulty'
+        }   
+    }
+}
+
+async function postLogs(user_id, word_id){
+    try {
+        const result = await postUserLogs(user_id, word_id);
+
+        if(result === 'fail'){
+            return {
+                'status': 'fail',
+                'message': 'gagal post logs'
+            };
+        }
+
+        return {
+            'status': 'success',
+	        'message': result
+        };
+
+    } catch (error) {
+        console.log(error);
+        return {
+            'status': 'fail',
+            'message': 'gagal post logs'
+        }   
+    }
+}
+
 async function userForgetPassword(email) {
     try {
         const result = await forgetPassword(email);
@@ -120,13 +245,15 @@ async function editUsername(user_id, username) {
     }
 }
 
-async function uploadProfilePic(user_id, file, filename){
+async function uploadProfilePic(user_id, file){
     try {
-        const file_extension = filename.slice(
-            ((filename.lastIndexOf('.') - 1) >>> 0) + 2
-        );
+        const base64String = file;
+        const file_extension = base64String.split(';')[0].split('/')[1];
+        const base64Data = base64String.split(',')[1];
+
+        const decodedData = buffer.from(base64Data, 'base64');
         
-        const result = await uploadUserProfilePic(user_id, file, file_extension);
+        const result = await uploadUserProfilePic(user_id, decodedData, file_extension);
         const resultSQL = await addUserProfilePic(user_id, result);
 
         if(result === 'fail' || resultSQL === 'fail'){
@@ -138,7 +265,8 @@ async function uploadProfilePic(user_id, file, filename){
 
         return {
             'status': 'success',
-            'message': 'berhasil upload profile picture'
+            'message': 'berhasil upload profile picture',
+            'data': result
         };
     } catch (error) {
         console.log(error);
@@ -173,9 +301,13 @@ async function ubahPassword(email, oldPassword, newPassword){
     }
 }
 
-module.exports = {  
+module.exports = { 
+    getWord, 
     loginUser, 
-    signupUser,
+    signupUser, 
+    updateProgress, 
+    getWordsByDifficulty, 
+    postLogs, 
     userForgetPassword,
     editUsername,
     uploadProfilePic,

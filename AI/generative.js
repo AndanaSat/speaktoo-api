7b755e6078aa6b52197e544e3ce93143038388cf
aud1tya4dnan <auditya.adnan2k3@gmail.com>
2024-06-17 12:09:07 +0700

    update works generative

---
+++ AI/generative.js
@@ -0,0 +1,62 @@
+// const {VertexAI} = require('@google-cloud/vertexai');
+import {VertexAI} from '@google-cloud/vertexai';
+
+// Initialize Vertex with your Cloud project and location
+const vertex_ai = new VertexAI({project: 'speaktoo-api', location: 'asia-southeast1'});
+const model = 'gemini-1.5-flash-001';
+
+// Instantiate the models
+const generativeModel = vertex_ai.preview.getGenerativeModel({
+  model: model,
+  generationConfig: {
+    'maxOutputTokens': 4000,
+    'temperature': 0.7,
+    'topP': 0.95,
+  },
+  safetySettings: [
+    {
+        'category': 'HARM_CATEGORY_HATE_SPEECH',
+        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
+    },
+    {
+        'category': 'HARM_CATEGORY_DANGEROUS_CONTENT',
+        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
+    },
+    {
+        'category': 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
+        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
+    },
+    {
+        'category': 'HARM_CATEGORY_HARASSMENT',
+        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
+    }
+  ],
+});
+
+async function generateContent(base64file, word) {
+    
+    const audio1 = {
+        inlineData: {
+            mimeType: 'audio/x-m4a',
+            data: base64file
+        }
+    };
+    
+    const text1 = {text: `\"you\'re an English teacher. how do you pronounce \"`+ word +`\" give me a brief explanation\"`};
+    
+    const req = {
+      contents: [
+        {role: 'user', parts: [audio1, text1]}
+      ],
+    };
+    
+  const streamingResp = await generativeModel.generateContentStream(req);
+
+  for await (const item of streamingResp.stream) {
+    process.stdout.write('stream chunk: ' + JSON.stringify(item) + '\n');
+  }
+
+  process.stdout.write('aggregated response: ' + JSON.stringify(await streamingResp.response));
+}
+
+export default generateContent;

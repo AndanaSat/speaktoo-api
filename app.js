a3f85e1a5d502d2cdc824fb8a7f974164366a4b0
AndanaSat <andana.s.her@gmail.com>
2024-06-17 17:31:35 +0700

    update ai + app.js

---
+++ app.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import multer from 'multer';
+import generateContent from './ai.js';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+}));
+
+const upload = multer({ 
+    storage: multer.memoryStorage() 
+});
+
+app.post('/generate', upload.single('audio'), async (req, res) => {
+    try {
+        const word = req.body.word;
+        const audioFile = req.file;
+        const base64file = audioFile.buffer.toString('base64');
+
+        const responseFormat = {
+            'word': 'the inputed word',
+            'phonetics': 'the phonetics for the word',
+            'feedback': 'an array with feedback of my pronunciation (the number of array is equal to the number of words that is inputted)'
+        };
+
+        const text1 = { text: `you're an English teacher. how do you pronounce "${word}" give me feedback on my pronunciation in the audio, please send a JSON response with this format: ${JSON.stringify(responseFormat)}` };
+
+        const audio1 = {
+            inlineData: {
+                mimeType: 'audio/mpeg',
+                data: base64file
+            }
+        };
+
+        const result = await generateContent(audio1, text1);
+        const response = JSON.parse(result.candidates[0].content.parts[0].text.replace(/```json\n|```/g, ''));
+
+        res.status(200).json({
+            'status': 'success',
+            'result': response
+        });
+
+    } catch (error) {
+        console.error('Error generating content:', error);
+        res.status(500).json({ status: 'fail', message: 'Error generating content' });
+    }
+});
+
+const port = 4000;
+app.listen(port, () => {
+    console.log('Server is listening on', port);
+});

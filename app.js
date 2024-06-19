afac2e440999fca5b49bbcffa80768281862bef8
AndanaSat <andana.s.her@gmail.com>
2024-06-20 01:48:00 +0700

    update response

--- app.js
+++ app.js
@@ -23,13 +23,7 @@ app.post('/generate', upload.single('audio'), async (req, res) => {
         const audioFile = req.file;
         const base64file = audioFile.buffer.toString('base64');
 
-        const responseFormat = {
-            'word': 'the inputed word',
-            'phonetics': 'the phonetics for the word',
-            'feedback': 'an array with feedback of my pronunciation (the number of array is equal to the number of words that is inputted)'
-        };
-
-        const text1 = { text: `you're an English teacher. how do you pronounce "${word}" give me feedback on my pronunciation in the audio, please send a JSON response with this format: ${JSON.stringify(responseFormat)}` };
+        const text1 = { text: `you're an English teacher. how do you pronounce "${word}" give me feedback on my pronunciation in the audio` };
 
         const audio1 = {
             inlineData: {
